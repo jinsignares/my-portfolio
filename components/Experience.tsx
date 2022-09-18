d826129b8fc56e90386455dcb51117252516896c
Juan Insignares <juan.insignares@guarapo.dev>
2022-09-17 21:23:23 -0500

    scrollbar plugin

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,7 +12,7 @@ function Experience({ }: Props) {
             transition={{ duration: 1.5 }}
             className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-x-7xl px-10 mx-auto justify-evenly items-center'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
-            <div className="mt-20 w-full flex space-x-5 overflow-x-scroll p-10 snap-mandatory snap-x">
+            <div className="mt-20 w-full flex space-x-5 overflow-x-scroll p-10 snap-mandatory snap-x  scrollbar-track-gray-400/20 scrollbar-thumb-blue-400/80 scrollbar-thin">
                 <ExperienceCard />
                 <ExperienceCard />
                 <ExperienceCard />
